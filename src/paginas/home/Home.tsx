4f62fb17a69c6656008e8cc2bfc941ff05303f34
ChrisPCruz99 <christian.pcruz99@gmail.com>
2023-04-13 16:39:32 -0300

    New: Criação e estilização da tela de home e inclusão dos incones .svg

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import styles from './Home.module.css'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className={styles.content}>
+      <div className={styles.txtHome}>
+        <p>Seja bem vindo à Carteira do Zé</p>
+        <p>A carteira feita para te ajudar no controle financeiro!</p>
+      </div>
+      <div className={styles.home}>
+          <button type="submit">Entrar</button>
+        {/* <p>ou</p> */}
+          <button type="submit">Cadastrar</button>
+
+      </div>
+</div>
   )
 }
 
