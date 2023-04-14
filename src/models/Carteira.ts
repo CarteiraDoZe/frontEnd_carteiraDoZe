0e23983738fe049199935ca8eb5e7f48b665f28e
ChrisPCruz99 <christian.pcruz99@gmail.com>
2023-04-14 14:20:13 -0300

    New: Integração com o back-end no component CadastroCarteira.tsx

--- src/models/Carteira.ts
+++ src/models/Carteira.ts
@@ -3,6 +3,7 @@ import { Usuario } from "./Usuario";
 export interface Carteira{
     id: number
     meta: number
+    valorEntrega: number
     diaria: number
-    usuario?: Usuario
+    usuario?: Usuario | null
 }
