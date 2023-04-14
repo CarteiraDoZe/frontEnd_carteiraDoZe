d5b93566018ffc151543a782e6936547afe88c40
WagnerMor <wagnermor@outlook.com>
2023-04-13 22:34:39 -0300

    Feat: Models Transacao e Movimentacao

---
+++ src/models/Movimentacao.ts
@@ -0,0 +1,12 @@
+import { Transacao } from './Transacao';
+import { Carteira } from '.Carteira';
+
+export interface Movimentacao {
+  id: Number;
+  valorUnitario: Number;
+  dataMovimentacao: String;
+  descricao: String;
+  quilometragem: String;
+  carteira?: Carteira;
+  transacao?: Transacao;
+}
